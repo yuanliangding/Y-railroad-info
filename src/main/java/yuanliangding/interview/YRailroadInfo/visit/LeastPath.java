4a83c2ed282e0b5edc0a35d89b4b9a127d00da17
yuanliangding <yuanliangding@126.com>
2019-05-27 16:47:40 +0800

    LeastPath完成

---
+++ src/main/java/yuanliangding/interview/YRailroadInfo/visit/LeastPath.java
@@ -0,0 +1,127 @@
+package yuanliangding.interview.YRailroadInfo.visit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import yuanliangding.interview.YRailroadInfo.map.Stop;
+
+/** 
+ * @ClassName: LeastPath
+ * @Description:  权重总值最小的路径.如果选择的维度是路程dist,则就是最短路径.
+ * 						最短路径,其实是一个特殊的LimitedPath,
+ * 						只是在具体化路径时,向后续节点遍历,
+ * 						如果对于同一个结点,之前遍历过的路径是更优的,就中断当前分支的遍历
+ *
+ * @author 袁良锭(https://github.com/yuanliangding)
+ * @date 2019年5月27日-下午2:00:18
+ */
+public class LeastPath extends LimitedPath {
+	
+	private Map<Stop, List<TempPath>> tempPaths = new HashMap<>();
+
+	/**
+	 * @param begin	路线起点
+	 * @param end	路线终点
+	 * @param dim	描述针对具体维度的权重
+	 */
+	protected LeastPath(Stop begin, Stop end, String dim) {
+		super(begin, end, dim, 0, Integer.MAX_VALUE, true, true);	// TODO 对于规模大的地图,权重总值有可能溢出
+	}
+	
+	/**
+	 * 
+	 * */
+	@Override
+	protected boolean toBeContinue(TempPath tempPath) {
+		boolean result = true;
+		
+		Stop curr = tempPath.getCurr();
+		if (tempPaths.containsKey(curr)) {
+			List<TempPath> lastTempPaths = tempPaths.get(curr);
+			
+			int lastTotalWeight = lastTempPaths.get(0).getTotalWeight();
+			if (lastTotalWeight < tempPath.getTotalWeight()) {
+				result = false;
+			} else if (lastTotalWeight == tempPath.getTotalWeight()) {
+				lastTempPaths.add(tempPath);
+				result = false;				
+			} else {
+				lastTempPaths.clear();
+				lastTempPaths.add(tempPath);
+			}
+		} else {
+			List<TempPath> lastTempPaths = new ArrayList<>();
+			lastTempPaths.add(tempPath);
+			tempPaths.put(curr, lastTempPaths);
+		}
+		
+		// TODO 对于权重不会为负值,遍历已经到最终结点,就没有必要继续下去了.再绕一圈,总权重不可能变小.
+		if (tempPath.getCurr().equals(end)) {
+			result = false;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * */
+	@Override
+	protected List<CertainPath> getResult() {
+		
+		List<List<Stop>> tempResult = new ArrayList<>();
+		
+		tempPaths.remove(end).stream().forEach(tempPath -> {
+			List<Stop> tempList = new LinkedList<>();
+			tempResult.add(tempList);
+			
+			concatResult(tempList, tempPath, tempResult);
+		});
+		
+		return tempResult.stream()
+				.map(stopList -> new CertainPath(stopList))
+				.collect(Collectors.toList());
+	}
+	
+	/**
+	 * TODO 这里代码流程不够干净简洁
+	 * */
+	private void concatResult(List<Stop> resultList, TempPath tempPath, List<List<Stop>> tempResult) {
+		Stop curr = tempPath.getCurr();
+		List<TempPath> currTempPaths = tempPaths.get(curr);
+		if (currTempPaths == null || currTempPaths.size() == 1) {
+			resultList.add(0, curr);
+			if (tempPath.getPrevious() != null) {
+				concatResult(resultList, tempPath.getPrevious(), tempResult);
+			}
+		} else {
+			tempResult.remove(resultList);
+			currTempPaths.forEach(currTempPath -> {
+				List<Stop> copyResultList = new ArrayList<>();
+				Collections.copy(copyResultList, resultList);
+				
+				tempPaths.remove(curr);
+				
+				concatResult(copyResultList, currTempPath, tempResult);
+			});
+		}
+	}
+	
+	/**
+	 * 
+	 * */
+	@Override
+	protected void saveAsResult(TempPath tempPath) {}
+	
+	@Override
+	protected void clear() {
+		super.clear();
+		tempPaths.clear();
+	}
+
+}
---
+++ src/test/java/yuanliangding/interview/YRailroadInfo/visit/LeastPathTest.java
@@ -0,0 +1,55 @@
+package yuanliangding.interview.YRailroadInfo.visit;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import yuanliangding.interview.YRailroadInfo.map.Stop;
+import yuanliangding.interview.YRailroadInfo.reader.PlainTextMapReader;
+
+/** 
+ * @ClassName: LeastPathTest
+ *
+ * @author 袁良锭(https://github.com/yuanliangding)
+ * @date 2019年5月27日-下午4:13:29
+ */
+public class LeastPathTest extends PathTest {
+	
+	@Test
+	public void testConcrete() {
+		
+		Stop a = stopMap.getStop("A");
+		Stop b = stopMap.getStop("B");
+		Stop c = stopMap.getStop("C");
+		
+		LeastPath ac_dist = new LeastPath(a, c, PlainTextMapReader.DIST);
+		List<CertainPath> ac_dist_paths = ac_dist.concrete();
+		Assert.assertThat("从A到C,最短路径路线有1条", ac_dist_paths.size(), CoreMatchers.equalTo(1));
+		Assert.assertThat(
+				"从A到C,最短路径路线有1条:A-B-C", 
+				ac_dist_paths.stream().map(CertainPath::toString).collect(Collectors.toList()), 
+				CoreMatchers.hasItems("A-B-C")
+				);
+		Assert.assertThat(
+				"从A到C,最短路径距离为9", 
+				ac_dist_paths.get(0).getTotalWeight(PlainTextMapReader.DIST), 
+				CoreMatchers.equalTo(9));
+		
+		LeastPath bb_dist = new LeastPath(b, b, PlainTextMapReader.DIST);
+		List<CertainPath> bb_dist_paths = bb_dist.concrete();
+		Assert.assertThat("从B到B,最短路径路线有1条", bb_dist_paths.size(), CoreMatchers.equalTo(1));
+		Assert.assertThat(
+				"从B到B,最短路径路线有1条:B-C-E-B", 
+				bb_dist_paths.stream().map(CertainPath::toString).collect(Collectors.toList()), 
+				CoreMatchers.hasItems("B-C-E-B")
+				);
+		Assert.assertThat(
+				"从A到C,最短路径距离为 9", 
+				bb_dist_paths.get(0).getTotalWeight(PlainTextMapReader.DIST), 
+				CoreMatchers.equalTo(9));
+		}
+
+}
