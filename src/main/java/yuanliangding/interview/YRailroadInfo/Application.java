011d8c381d851574c368288efd1b521c9beeb19e
yuanliangding <yuanliangding@126.com>
2019-05-28 10:42:45 +0800

    整理代码

--- src/main/java/yuanliangding/interview/YRailroadInfo/Application.java
+++ src/main/java/yuanliangding/interview/YRailroadInfo/Application.java
@@ -1,8 +1,5 @@
 package yuanliangding.interview.YRailroadInfo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /** 
  * @ClassName: Application
  * @Description:  TODO
@@ -15,10 +12,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		Map<String,String> m = new HashMap<>();
-		m.put("Aa", "x");
-		m.put("BB", "y");
-		System.out.println(m);
 		// TODO Auto-generated method stub
 	}
 
