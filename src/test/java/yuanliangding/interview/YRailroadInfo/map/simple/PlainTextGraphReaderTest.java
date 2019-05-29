d675815b06e302e9592db520eb5f31a9b41e8ca8
yuanliangding <yuanliangding@126.com>
2019-05-29 19:49:03 +0800

    PlainTextGraphReaderTest单元测试改成,基于TWDataProvider

--- src/test/java/yuanliangding/interview/YRailroadInfo/map/simple/PlainTextGraphReaderTest.java
+++ src/test/java/yuanliangding/interview/YRailroadInfo/map/simple/PlainTextGraphReaderTest.java
@@ -1,14 +1,9 @@
 package yuanliangding.interview.YRailroadInfo.map.simple;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import yuanliangding.interview.YRailroadInfo.graph.GraphReader.GraphEdge;
@@ -19,35 +14,14 @@ import yuanliangding.interview.YRailroadInfo.graph.GraphReader.GraphEdge;
  * @author 袁良锭(https://github.com/yuanliangding)
  * @date 2019年5月27日-上午12:26:56
  */
-public class PlainTextGraphReaderTest {
-
-	private String textPath = null;
-	
-	private PlainTextGraphReader plainTextGraphReader = null;
-
-	@Before
-	public void before() throws IOException {
-		File mapTextFile = File.createTempFile("y_railroad_info_map_plain_text", ".txt");
-		textPath = mapTextFile.getCanonicalPath();
-		try (FileWriter fileWriter = new FileWriter(mapTextFile)) {
-			fileWriter.write("AB5\nBC4\nCD8\n");
-		}
-		
-		plainTextGraphReader = new PlainTextGraphReader(textPath);
-	}
-
-	@After
-	public void after() {
-		File mapTextFile = new File(textPath);
-		mapTextFile.deleteOnExit();
-	}
+public class PlainTextGraphReaderTest extends TWDataProvider{
 
 	@Test
 	public void testRead() {
-		List<GraphEdge> results = plainTextGraphReader.read();
+		List<GraphEdge> results = graphReader.read();
 		
 		int count = results.size();
-		Assert.assertThat("总共读取了3条数据才对", count, CoreMatchers.equalTo(3));
+		Assert.assertThat("总共读取了9条数据才对", count, CoreMatchers.equalTo(9));
 		
 		GraphEdge graphEdge0 = results.get(0);
 		Assert.assertThat("验证AB5出错",graphEdge0.getStart(), CoreMatchers.equalTo("A"));
