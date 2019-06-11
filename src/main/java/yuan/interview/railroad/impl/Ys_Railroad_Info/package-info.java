b17881aff53b6c6487d412f7df6250a871c8e5e3
yuanliangding <yuanliangding@126.com>
2019-06-11 23:50:49 +0800

    Ys_Railroad_Info初始

---
+++ src/main/java/yuan/interview/railroad/impl/Ys_Railroad_Info/package-info.java
@@ -0,0 +1,8 @@
+/** 
+ * @ClassName: package-info
+ * @Description:  Y_Railroad_Info系统的升级版，做为重构的基础预写部分。
+ *
+ * @author 袁良锭(https://github.com/yuanliangding)
+ * @date 2019年6月11日-下午11:48:55
+ */
+package yuan.interview.railroad.impl.Ys_Railroad_Info;
